81271bba81c7091ca14b335034a4139041326ab5
iDavi <69827032+iDavi@users.noreply.github.com>
2020-08-26 11:38:03 -0300

    O Script
    uau o script

---
+++ Covid-19-dados.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def RetornarResposta(req):
+    print("0 = Visão geral")
+    print ("1 = Casos ativos no mundo")
+    print("3 = Casos leves ativos no mundo")
+    print("5 = Casos severos ativos no mundo")
+    modos = ["Mostrando visão geral","Mostrando casos ativos no mundo","",
+    "Mostrando casos leves ativos no mundo","","Mostrando Casos severos ativos no mundo"]
+
+    modo = int(input("Digite um número"))
+    content = req.content
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find(name='table')
+    table_str = str(table)
+    df = pd.read_html(table_str)[0]
+
+    print(modos[modo])
+    if not modo == 0: 
+        print(df[0][modo])
+    else:
+        print(df[0])
+
+
+def Iniciar():
+    print("Conectando com o banco de dados...")
+    req = requests.get("https://www.worldometers.info/coronavirus/coronavirus-cases/")
+    if req.status_code == 200:
+        print("Conectado!")
+        print("Dados da Covid 19 no mundo - Python | by @iDavi")
+        RetornarResposta(req)
+    else:
+        print("Erro ao tentar se conectar ao banco de dados")
+    
+
+Iniciar() #Apenas chame iniciar para iniciar o programa
+
